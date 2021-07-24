896f461ce740b56d8e16351f545f887904b2d2c4
Uzochukwu Ebube <uzoebubechukwu@gmail.com>
2021-07-24 23:47:15 +0100

    a whole lot of stuffs

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,16 @@ import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-import productRoutes from './routes/productRoutes.js'
+// import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
+// import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
-const app = express()
+const app = express() 
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.use('/api/products', productRoutes)
+// app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+// app.use('/api/orders', orderRoutes) 
 app.use('/api/upload', uploadRoutes)
 
 // app.get('/api/config/paypal', (res) =>
